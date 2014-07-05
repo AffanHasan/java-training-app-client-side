f93050f6e4eda513c68790dcb2346cca846d65c9
affan.hasan <affan.hasan@royalcyber.com>
2014-07-05 10:44:24 +0500

    Added styling to the tutoatial topic content DIV + added some mark up classes

--- java_training_app/web/engine/web_components/tutorial_topic/tutorial-topic.dart
+++ java_training_app/web/engine/web_components/tutorial_topic/tutorial-topic.dart
@@ -19,34 +19,17 @@ class TutorialTopic extends PolymerElement  {
 """
 <h1>JVM</h1>
 <hr>
-<p>
-  The JVM(JAVA Virtual Machine) is the platform upon which the <b>JAVA</b> based applications run.
-</p>
-<p>
-  JVM is a <span class='highlight bold'>process virtual machine</span>.
-</p>
-
-<img src='engine/images/Untitled.png'></img>
-
-<h2>Why On Earth The JVM Was Created?</h2>
+<p>The <span class='bold'>JVM</span> <span class='bold italic'>Java Virtual Machine</span> is an <span class='highlight bold'>application software</span> which forms the platform for the JAVA related technology</p>
+<p>The <span class='bold'>JVM</span> is <span class='bold'>responsible for</span> the <span class='highlight bold'><span class='bold'>hardware</span> & <span class='bold'>operating system</span> independence</span> of the <span class='bold'>JAVA<span> programming language</p>
+<p>The <span class='bold'>JVM</span> is an <span class='highlight bold'>abstract computing machine</span></p>
+<h1>Why Do I Need JVM...? WHY OOO WHY...?</h1>
 <hr>
-<dl>
-
-  <dt>A Variety Of Different Platforms Was The Real Pain...</dt>
-  <dd>
-By the time JAVA was created there were various different platforms for the consumer electronics devices.
-
-  </dd>
-
-  <dt>Ability To <span class='bold italic highlight'>Write Once Run Every Where</span> Was Badly Needed</dt>
-  <dd>
-     ...
-  </dd>
-</dl>
-<h2>JVM Languages</h2>
+<p>Before the <span class='bold'>JAVA programming language</span> & <span class='bold'>JVM</span> there were other <span class='bold'>compiled</span> languages such as <span class='bold italic'>C, C++, Python...</span> etc</p>
+<h1>JVM Knows Nothing About JAVA Programming Language, <span class='italic'>STRANGE...!</span></h1>
 <hr>
+<p>The <span class='bold'>JVM</span> knows about only one <span class='highlight bold'>instruction set</span>; we call it the <span class='bold italic highlight'>BYTE CODE !</span></p>
 """
-             };
+};
   
 //Default Constructor
   TutorialTopic.created() : super.created(){
